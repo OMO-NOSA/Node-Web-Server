3209a89f620225649bc30632649bc2361f91ac77
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-04-22 16:57:19 +0100

    Worked with Express Middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const hbs = require('hbs');
 const app = express();
 const port = 3000;
@@ -6,6 +7,26 @@ const port = 3000;
 
 hbs.registerPartials(__dirname + '/views/partials/');
 app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}, ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+})
+
+app.use((req, res, next) => {
+    res.render('/maintenance.hbs');
+})
+
 app.use(express.static(__dirname + '/Public'));
 
 hbs.registerHelper('getCurrentYear', () => {
