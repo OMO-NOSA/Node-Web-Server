c3174170d3cd472fd171b0544a3fbdace0ec35f1
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-04-22 14:47:04 +0100

    Worked with handlebar partials and helpers

--- server.js
+++ server.js
@@ -3,26 +3,32 @@ const hbs = require('hbs');
 const app = express();
 const port = 3000;
 
+
+hbs.registerPartials(__dirname + '/views/partials/');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/Public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     //res.send('<h1>Hello World!</h1>'); 
-    res.render({
+    res.render('home.hbs', {
         pageTitle: 'Home Page',
         welcomeMessage: 'Welcome to my webpage',
-        likes: [
-                'Reading',
-                'Trancedental Meditation',
-                'Hoeing'
-            ]
-            //
+
     })
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        name: 'Nosa Omorodion Japhet'
     })
 });
 //
